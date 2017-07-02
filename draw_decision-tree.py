22ce9af7ec4649f87c57bddef7447cd5d86c0b8e
jdemonasterio <laterio@gmail.com>
2017-07-01 22:50:55 -0300

    minor additions

--- draw_decision-tree.py
+++ draw_decision-tree.py
@@ -10,24 +10,37 @@ In ubuntu use `sudo apt-get install graphviz
 And then run this script below.
 """
 import subprocess
+import os
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn import tree
 
+REPO_DIR = os.path.dirname(os.path.realpath(__file__))
+# REPO_DIR = "/".join(REPO_DIR[:REPO_DIR.index('Tesis')+1])
+
+PROJECT_DIR = os.path.join(REPO_DIR,'mexico-scripts-ver2')
+DATA_DIR = os.path.join(PROJECT_DIR,'data')
+
 clf = tree.DecisionTreeClassifier()
+input_file =  os.path.join(DATA_DIR,'data_balanced_sample.csv')
+
 iris = load_iris()
 
+df = pd.DataFrame(iris.data)
+df['y'] = iris.target
 
-clf = clf.fit(iris.data, iris.target)
+clf = clf.fit(df, iris.target)
 
 outf = 'tree.dot'
 tree.export_graphviz(clf,
                     max_depth = 7,
-impurity = True,
-            out_file=outf)
+                        impurity = True,
+                        proportion = True,
+                    out_file=outf)
 
-sub_call = 'dot -Tps {} -o {} '.format(outf, outf.replace('.dot','.png'))
-# print(sub_call)
-subprocess.call(sub_call, shell=True)
+sub_call = 'dot -Tps {} -o {} '.format(outf, outf.replace('.dot','.ps'))
+print(sub_call)
+# subprocess.call(sub_call, shell=True)
 
 
 
